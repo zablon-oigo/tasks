77aed4d07684623c27fd2c79f1e12d708f1ccdf4
zablon <zablonoigo51@gmail.com>
2024-02-29 16:33:38 +0300

    setting up test data

--- task/tests.py
+++ task/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Task
+from django.urls import reverse
 
-# Create your tests here.
+
+class TastTest(TestCase):
+    @classmethod
+    def SetUpTestData(cls):
+        cls.user=User.objects.create_user(
+            username='testiser',
+            email='testuser@mail.com',
+            password='secret'
+        )
+        cls.task=Task.objects.create(
+            title='New task',
+            slug='new-task',
+            body='testing task app',
+            status='pending',
+            created_by=cls.user
+        )
